a194febd6d81317f92bea4fd9a614997c59e4fb2
Gaurav Dahal <gauravdahal53@gmail.com>
2020-07-09 08:58:28 +0545

    09-07-2020

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv=(RecyclerView) findViewById(R.id.recycler);
+        int images[] = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.fidha,R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j};
+
+        String names[]={"Gaurav","Ujjwol","Biraj","Sanju","Indra","Nilesh","Prayash","Parshal","Sonika","Saronee"};
+
+
+        rv.setLayoutManager(new LinearLayoutManager((this)));
+        MyAdapter adapter=new MyAdapter(this,images,names);
+        rv.setAdapter(adapter);
+
+
+    }
+}
